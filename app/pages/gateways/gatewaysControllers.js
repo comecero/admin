1a98f1d988ef8d8ba3e1b9b3e5522e228c0a3dcf
Chuck Larson <chuck-github@chlarson.com>
2019-02-07 19:39:01 -0600

    Revert "updated gateways to handle multiple payment methods per gateway"
    This reverts commit c25f0a0eac0c4164b68bfebe4e661fb6d9c37d3a.

--- app/pages/gateways/gatewaysControllers.js
+++ app/pages/gateways/gatewaysControllers.js
@@ -27,7 +27,7 @@ app.controller("GatewaysListCtrl", ['$scope', '$routeParams', '$location', '$q',
 
     $scope.loadGateways = function () {
 
-        ApiService.getList(ApiService.buildUrl("/gateways?show=gateway_id,name,active,payment_method_types,date_created&sort_by=" + $scope.params.sort_by + "&desc=" + $scope.params.desc), $scope.params).then(function (result) {
+        ApiService.getList(ApiService.buildUrl("/gateways?show=gateway_id,name,active,payment_method_type,date_created&sort_by=" + $scope.params.sort_by + "&desc=" + $scope.params.desc), $scope.params).then(function (result) {
             $scope.gateways.gatewayList = result;
             $scope.gatewaysChecked = false;
 
@@ -204,6 +204,7 @@ app.controller("GatewaysSetCtrl", ['$scope', '$routeParams', '$location', 'Growl
         ApiService.getItem(ApiService.buildUrl("/gateways/options")).then(function (gatewayOptions) {
 
             $scope.gateway_configs = gatewayOptions.gateway_configs;
+            //$scope.payment_method_types = _.uniq(_.pluck($scope.gateway_configs, "payment_method_type"));
             $scope.currencies = gatewayOptions.currencies;
             $scope.card_types = gatewayOptions.card_types;
 
@@ -289,7 +290,7 @@ app.controller("GatewaysSetCtrl", ['$scope', '$routeParams', '$location', 'Growl
         $scope.gateway_config = _.findWhere($scope.gateway_configs, { provider_id: $scope.gateway.provider_id });
 
         if ($scope.gateway_config.payment_method_types.length == 1) {
-            $scope.gateway.payment_method_types = angular.copy($scope.gateway_config.payment_method_types);
+            $scope.gateway.payment_method_type = $scope.gateway_config.payment_method_types[0];
         }
 
         $scope.loadFields(provider_id);
@@ -343,24 +344,15 @@ app.controller("GatewaysSetCtrl", ['$scope', '$routeParams', '$location', 'Growl
     }
 
     $scope.supportsCreditCard = function (gateway) {
-        return $scope.isPaymentMethodSelected(gateway, "credit_card");
-    }
 
-    $scope.isPaymentMethodSelected = function (gateway, payment_method) {
-        if (gateway && angular.isArray(gateway.payment_method_types)) {
-            return gateway.payment_method_types.indexOf(payment_method) >= 0;
+        if (gateway) {
+            if (gateway.payment_method_type == "credit_card") {
+                return true;
+            }
         }
+
         return false;
-    }
 
-    $scope.togglePaymentMethod = function (gateway, payment_method) {
-        if (!angular.isArray(gateway.payment_method_types)) gateway.payment_method_types = [];
-        var idx = gateway.payment_method_types.indexOf(payment_method);
-        if (idx >= 0) {
-            gateway.payment_method_types.splice(idx, 1);
-        } else {
-            gateway.payment_method_types.push(payment_method);
-        }
     }
 
     $scope.confirmDelete = function () {
